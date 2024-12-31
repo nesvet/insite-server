8275b0f94130a1bede2337c4b43e6a1281db112a
Eugene Nesvetaev <dev@nesvet.dev>
2024-12-31 16:14:34 +0400

    feat: Re-export server-side insite stuff

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 export * from "./InSite";
-export { Options } from "./types";
+export type { Options } from "./types";
+export * from "insite-common";
+export * from "insite-cookie/server";
+export type { Options as CookieSetterOptions } from "insite-cookie/server";
+export * from "insite-db";
+export { connect as connectToDB, type Options as DBOptions } from "insite-db";
+export * from "insite-http";
+export type { Options as HTTPServerOptions } from "insite-http";
+export * from "insite-subscriptions-server";
+export * from "insite-users-server";
+export type { Options as UsersOptions } from "insite-users-server";
+export * from "insite-users-server-ws";
+export type { Options as UsersServerOptions } from "insite-users-server-ws";
+export * from "insite-ws/server";
+export type { Options as WSServerOptions } from "insite-ws/server";
+export * from "insite-ws-transfers";
